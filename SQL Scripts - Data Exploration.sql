a6a992a8d5887b292b01d9c40ac1026e430da423
crisdandre <143665434+crisdandre@users.noreply.github.com>
2023-10-18 16:31:18 -0400

    Update SQL Scripts - Data Exploration.sql

--- SQL Scripts - Data Exploration.sql
+++ SQL Scripts - Data Exploration.sql
@@ -1,17 +1,17 @@
---Overview of the complete Dataset 
+-- Dataset (initial view)
 
 SELECT *
 FROM [Project 2 - Covid Data].[dbo].[CovidData]
 
 
---Filtering columns that I'm going to be using for cases/deaths 
+-- Filtering columns that I'm going to be using for cases/deaths 
 
 Select Location, date, total_cases, new_cases, total_deaths, population
 From [Project 2 - Covid Data]..[CovidData]
 Order by Location, date
 
 
---Looking at Total cases vs Total deaths change over time around the world, and in Spain
+-- Looking at Total cases vs Total deaths change over time around the world, and in Spain
 
 Select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as Deaths_percentage
 From [Project 2 - Covid Data]..[CovidData]
@@ -24,8 +24,8 @@ Where location='Spain'
 Order by date
 
 
---Looking at Total cases vs Population to check the percentage of infected individuals over time around the world, and in Spain
---Note: total_cases was multiplied by "1." to transform the data from integer to numeric
+-- Selecting Total cases vs Population to check the percentage of infected individuals over time around the world, and in Spain
+-- Note: total_cases was multiplied by "1." to transform the data from integer to numeric
 
 Select Location, date, total_cases, Population, (total_cases*1./Population)*100 as Infected_percentage
 From [Project 2 - Covid Data]..[CovidData]
@@ -37,7 +37,7 @@ From [Project 2 - Covid Data]..[CovidData]
 Where location='Spain'
 Order by date
 
--- Looking at Countries with Highest Infection Rate compared to Population
+-- Countries with Highest Infection Rate compared to Population
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount, MAX((total_cases*1./Population)*100) 
 	   as HighestInfected_percentage
@@ -47,14 +47,16 @@ Group by Location, Population
 Order by HighestInfected_percentage desc
 
 
--- Looking at Countries with Highest Deaths Count
+-- Countries with Highest Deaths Count
 
 Select Location, MAX(total_deaths) as HighestDeathCount
 From [Project 2 - Covid Data]..[CovidData]
 Where continent IS NOT NULL and location <> 'European Union'
 Group by Location
 Order by HighestDeathCount desc
+	
 -- Breaking down the Highest Deaths Count by Continent/Worldwide
+	
 Select continent, MAX(total_deaths) as HighestDeathCount
 From [Project 2 - Covid Data]..[CovidData]
 Where continent IS NOT NULL
@@ -63,7 +65,7 @@ Order by HighestDeathCount desc
 
 
 -- Global daily cases and deaths
---Note: total_cases was multiplied by "1." to transform the data from integer to numeric
+-- Note: total_cases was multiplied by "1." to transform the data from integer to numeric
 
 Select date, SUM(new_cases) as TotalCases, SUM(new_deaths) as TotalDeaths, 
 	  (SUM(new_deaths*1.)/SUM(new_cases))*100 as DeathRate
@@ -71,6 +73,7 @@ From [Project 2 - Covid Data]..[CovidData]
 Where continent IS NOT NULL and location <> 'European Union'
 Group by date
 Order by date, TotalCases
+	
 -- Worldwide summary of Total cases and deaths 
 Select SUM(new_cases) as TotalCases, SUM(new_deaths) as TotalDeaths, 
 	  (SUM(new_deaths*1.)/SUM(new_cases))*100 as DeathRate
@@ -79,12 +82,14 @@ Where continent IS NOT NULL and location <> 'European Union'
 
 
 --Looking at worldwide Vaccination vs Population over time, and in Spain
+	
 Select continent, location, date, population, new_vaccinations, 
        SUM(new_vaccinations) OVER (PARTITION BY location order by location, date) as RollingPeopleVaccinated
 From [Project 2 - Covid Data].[dbo].[CovidData]
 Where continent IS NOT NULL and location <> 'European Union'
 Order by location, date
---Adding Vaccination Rate column
+	
+-- Adding Vaccination Rate column and checking worldwide and Spain data
 with CTE_PopVsVac as (
 Select continent, location, date, population, new_vaccinations, 
        SUM(new_vaccinations) OVER (PARTITION BY location order by location, date) as RollingPeopleVaccinated
@@ -107,7 +112,7 @@ Where location='Spain'
 Order by date
 
 
---Creating a TEMP table for the previous query
+-- Creating a TEMP table for the previous query
 
 CREATE TABLE #PeopleVaccinations (
 	continent nvarchar(50), 
@@ -129,7 +134,7 @@ From #PeopleVaccinations
 Order by location, date
 
 
---Looking at Countries with Highest Vaccination Rate using TEMP table #PeopleVaccinations
+-- Countries with Highest Vaccination Rate using TEMP table #PeopleVaccinations
 
 Select location, MAX(RollingPeopleVaccinated) AS RollingPeopleVaccinated, 
 	   MAX((RollingPeopleVaccinated*1./population)*100) as VaccinationRate
@@ -138,14 +143,7 @@ Group by location
 Order by VaccinationRate desc
 
 
---Global Total Vaccinations
+-- Global Total Vaccinations
+	
 Select SUM(new_vaccinations) as TotalVaccinations
 fROM #PeopleVaccinations
-
-
-
-
-
-
-
-
